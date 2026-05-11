a910cc01909689497965ce46dab7e2372a14b7dc
Mukesh101-lab <mukeshpatel314894@gmail.com>
2026-05-11 14:36:17 +0530

    update repo

--- MajorProject/aap.js
+++ MajorProject/aap.js
@@ -5,6 +5,8 @@ const Listing = require('./modals/listing')
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
 
 const port = 8080;
 
@@ -32,49 +34,59 @@ app.get('/',(req,res) =>{
 
 
 //show all listings
-app.get('/listings', async(req, res) => {
+app.get('/listings',wrapAsync(async(req, res) => {
     const listings = await Listing.find();
-    res.render('index.ejs', { listings });
-});
+    res.render('listings/index.ejs', { listings });
+}));
 
 //new listing form
 app.get('/listings/new', (req, res) => {
-    res.render('new.ejs');
+    res.render('listings/new.ejs');
 });
 
-app.post('/listings', async(req, res) => {
+app.post('/listings', wrapAsync(async (req, res, next) => {
     let newlisting = new Listing(req.body.listing);
     await newlisting.save();
     res.redirect('/listings');
-});
+}));
 
 
 //show single listing
-app.get('/listings/:id', async(req, res) => {
+app.get('/listings/:id',wrapAsync(async(req, res) => {
     let listing = await Listing.findById(req.params.id);
-    res.render('show.ejs', { listing });
-});
+    res.render('listings/show.ejs', { listing });
+}));
 
 
 //edit route
-app.get('/listings/:id/edit', async(req, res) => {
+app.get('/listings/:id/edit',wrapAsync(async(req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render('edit.ejs', { listing });
+    res.render('listings/edit.ejs', { listing });
 
-});
+}));
 
-app.put('/listings/:id', async(req, res) => {
+app.put('/listings/:id',wrapAsync(async(req, res) => {
     const { id } = req.params;
     await Listing.findByIdAndUpdate(id, req.body.listing);
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //delete route
-app.delete('/listings/:id',async(req,res) =>{
+app.delete('/listings/:id',wrapAsync(async(req,res) =>{
     const {id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect('/listings');
+}));
+
+app.all("*",(req,res,next) =>{
+    next(new ExpressError(404,"Page Not Found!"));
+});
+
+//middleware
+app.use((err,req,res,next) =>{
+    let {statusCode=500,message="something went wrong"} = err;
+    res.status(statusCode).send(message);
 });
 
 
