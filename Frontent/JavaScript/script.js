d6db8fed68406df52f6724d887a672010524c3a1
Mukesh101-lab <mukeshpatel314894@gmail.com>
2026-05-28 21:58:28 +0530

    update repo

--- Frontent/JavaScript/script.js
+++ Frontent/JavaScript/script.js
@@ -60,3 +60,157 @@
 // else{
 //     console.log(`Price is : ${100}`)
 // }
+
+// let str = "ango";
+// if(str[0] === 'a' && str.length > 3){
+//     console.log("good String");
+// }else{
+//     console.log("not good string");
+// }
+
+// let color = "green";
+
+// switch(color){
+//     case "red" : {
+//         console.log("Stop");
+//     }
+//     case "yellow" : {
+//         console.log("slow");
+//     }
+//     case "green" : {
+//         console.log("go");
+//     }
+// }
+
+// let day = '10';
+
+// switch(day){
+//     case '1' : {
+//         console.log("Monday")
+//         break;
+//     }
+//     case '2' :{
+//         console.log("Tuesday");
+//         break;
+//     }
+//     case '3' :{
+//         console.log("wednesday");
+//         break;
+//     }
+//     case '4 ' : {
+//         console.log("thursday");
+//         break
+//     }
+//     case '5' : {
+//         console.log("Friday");
+//         break;
+//     }
+//     case '6' : {
+//         console.log("saturday");
+//         break;
+//     }
+//     case '7' : {
+//         console.log("sunday");
+//         break;
+//     }
+//     default : {
+//         console.log("wrong day");
+//         break;
+//     }
+// }
+
+// alert("wrong paste");
+// let name = prompt("how are you : ");
+// console.log(name);
+
+// let firstName = prompt("enter first name : ");
+// let lastName = prompt("enter last name  : ");
+
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// let a = '32';
+// let b = '4285';
+
+// if(a[a.length-1] === b[b.length-1]){
+//     console.log("last digit same");
+// }else{
+//     console.log("not same last digit");
+// }
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+
+// if(a > b && a > c){
+//     console.log("a is large");
+// }
+// else if(b > c){
+//     console.log("b is large");
+// }
+// else{
+//     console.log("c is large");
+// }
+
+// let str = "    Mukesh   Patel  ";
+// console.log(str);
+// console.log(str.trim());
+
+// let str = "apnaCollege";
+// let str = "mukesh";
+
+// let student = ["aman","mukesh"];
+
+// let mounth = ["january","july","march","august"];
+
+// let color = ["pink","orange","yellow","red","green","purple","white"];
+
+// for(let i=1;i<=15;i+=2){
+//     console.log(i);
+// }
+
+
+// let favMovie = "avatar";
+// let guess = prompt("Guess Movie : ");
+
+// while(true){
+//     if(guess == "quit"){
+//         console.log("you quit");
+//         break;
+//     }
+//     else if(guess == favMovie){
+//         console.log("wow nice");
+//         break;
+//     }
+//     guess = prompt("wrong please try again");
+// }
+
+
+// let todo = [];
+
+// let req = prompt("please enter your request : ");
+
+// while(true){
+//     if(req == "quit"){
+//         console.log('To exit the todo');
+//         break;
+//     }
+//     if(req == "list"){
+//         console.log("--------------------");
+//         for(task of todo){
+//             console.log(task);
+//         }
+//         console.log("----------------------");
+//     }
+//     else if(req == "add"){
+//         let task = prompt("enter list add");
+//         todo.push(task);
+//         console.log("task added");
+//     }
+//     else if(req == "delete"){
+//         todo.pop();
+//         console.log("delete task");
+//     }
+//     req = prompt("please enter your request : ");
+// }
+
