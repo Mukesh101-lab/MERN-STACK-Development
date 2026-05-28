926ac67b37ace209b9870f4c3255ff0600375624
Mukesh101-lab <mukeshpatel314894@gmail.com>
2026-05-28 10:19:54 +0530

    update repo

---
+++ Frontent/JavaScript/script.js
@@ -0,0 +1,62 @@
+// console.log("Hellow Mukesh");
+
+// let a = 10;
+// let b = 5;
+// console.log(`Total Price is : ${a + b}`);
+
+//Arithmetic Operator
+// let a = 10;
+// let b = 5;
+
+// console.log(`sum : ${a + b}`);
+// console.log(`sub : ${a - b}`);
+// console.log(`mul : ${a * b}`);
+// console.log(`div : ${a / b}`);
+// console.log(`mod : ${a % b}`);
+// console.log(`exp : ${a ** b}`);
+
+//Comparison Operator
+// console.log(a > b);
+// console.log(a >= b);
+// console.log(a < b);
+// console.log(a <= b);
+// console.log(a == b);
+// console.log(a != b);
+
+//Conditional Statement
+
+//Trafic Light 
+// let color = "yelw";
+// if(color === "red"){
+//     console.log("Stop");
+// }
+// else if(color === "yellow"){
+//     console.log("slow")
+// }
+// else if(color === "green"){
+//     console.log("go");
+// }
+// else{
+//     console.log("Light is Broken");
+// }
+
+// let age = 20;
+// if(age >= 25){
+//     console.log("you can vote");
+// }else if(age < 25){
+//     console.log("you con't vote");
+// }
+
+// let size = 'S';
+// if(size === 'XL'){
+//     console.log(`Price is : ${250}`)
+// }
+// else if(size === 'L'){
+//     console.log(`Price is : ${200}`)
+// }
+// else if(size === 'M'){
+//     console.log(`Price is : ${150}`)
+// }
+// else{
+//     console.log(`Price is : ${100}`)
+// }
