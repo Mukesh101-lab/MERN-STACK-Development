fac6eccec81382a0ff0baa8a1b76d87e96ab008e
Mukesh101-lab <mukeshpatel314894@gmail.com>
2026-06-01 12:07:59 +0530

    update repo

--- Frontent/JavaScript/DOM/app.js
+++ Frontent/JavaScript/DOM/app.js
@@ -1,5 +1,33 @@
-let smallImage = document.getElementsByClassName("oldImg");
+// let smallImage = document.getElementsByClassName("oldImg");
 
-for(let i = 0; i < smallImage.length; i++){
-    console.log(smallImage[i].src);
-};
+// for(let i = 0; i < smallImage.length; i++){
+//     console.log(smallImage[i].src);
+// };
+
+// let img = document.getElementById("mainImg");
+// let para = document.getElementById("p");
+// let oldImg = document.getElementsByClassName("oldImg");
+// let list = document.getElementsByTagName("li");
+
+// img.src = "assets/creation_1.png";
+// console.log(img);
+// console.log(para);
+
+// para.innerText = "abcd";
+
+// for(let i=0;i<oldImg.length;i++){
+//     oldImg[i].src = "assets/spiderman_img.png";
+//     console.log(`image no ${i} is change`);
+// }
+
+// for(let i=0;i<list.length;i++){
+//     console.log(list.innerText);
+// }
+
+// let para = document.querySelector("p");
+
+let list = document.querySelectorAll(".box a");
+
+for(let i=0;i<list.length;i++){
+    list[i].style.color = 'red';
+}
---
+++ Frontent/JavaScript/PracticeDOM/script.js
@@ -0,0 +1,33 @@
+// let heading = document.getElementById('heading');
+// let heading = document.getElementById("heading");
+// console.log(heading);
+
+// let paras = document.getElementsByClassName('para');
+
+// for(para of paras){
+//     console.log(para.innerText);
+// }
+
+// let paras = document.getElementsByClassName("para");
+// console.log(paras);
+
+// let allP = document.getElementsByTagName("p");
+// console.log(allP);
+
+// let div = document.querySelector('.box');
+
+// let box = document.getElementById("box1");
+
+// let li = document.querySelector("li");
+
+// console.log(li.parentElement);
+
+// let ul = document.getElementById("list");
+
+// console.log(ul.children);
+
+// console.log(ul.firstElementChild);
+
+// console.log(ul.lastElementChild);
+
+// console.log(ul.nextElementSibling);
